c70f8cbe8311c07df851abdba7f93efe5f421d8b
= <=>
2023-05-01 15:16:17 -0500

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Confiho.MVVM.View;
+
+namespace Confiho;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new Login();
+	}
+}
---
+++ MVVM/View/Login.xaml.cs
@@ -0,0 +1,33 @@
+namespace Confiho.MVVM.View;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Pressed(object sender, EventArgs e)
+    {
+		ButtonLogin.BackgroundColor = Colors.BlueViolet;
+        ButtonLogin.TextColor = Colors.White;
+    }
+
+    private void Button_Pressed_1(object sender, EventArgs e)
+    {
+        ButtonRegister.BackgroundColor = Colors.Purple;
+        ButtonRegister.TextColor = Colors.White;
+    }
+
+    private void ButtonLogin_Released(object sender, EventArgs e)
+    {
+        ButtonLogin.BackgroundColor = Colors.Transparent;
+        ButtonLogin.TextColor = Colors.BlueViolet;
+    }
+
+    private void ButtonRegister_Released(object sender, EventArgs e)
+    {
+        ButtonRegister.BackgroundColor = Colors.Transparent;
+        ButtonRegister.TextColor = Colors.Purple;
+    }
+}
